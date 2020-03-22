55ee463485b676c597e9d436bd3801eb8273fb61
yasin vural <yasinvural0@gmail.com>
2020-03-22 22:17:17 +0300

    total values are added to the header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="header-container">
     <h2>{{ new Date(generalInfo.lastUpdate) }}</h2>
+    <Statistic :countryData="generalInfo" />
   </div>
 </template>
 
 <script>
+import Statistic from "./Statisctic";
+
 export default {
   name: "Header",
   props: {
     generalInfo: Object
+  },
+  components: {
+    Statistic
   }
 };
 </script>
@@ -17,8 +23,9 @@ export default {
 .header-container {
   display: flex;
   justify-content: center;
-  align-items: center;
   padding: 1rem;
   border: 1px solid #ececec;
+  flex-direction: column;
+  text-align: center;
 }
 </style>
