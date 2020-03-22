73a755298006e7085464a82467f3fae6af0d9fef
yasin vural <yasinvural0@gmail.com>
2020-03-22 18:09:08 +0300

    axios and antd are added as dependencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import { Button } from "ant-design-vue";
+import App from "./App.vue";
+import "ant-design-vue/dist/antd.css";
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+Vue.use(Button);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
