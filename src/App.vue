f501917bb11abba3db6cd50992bc068f856a3c59
yasin vural <yasinvural0@gmail.com>
2020-03-22 18:28:42 +0300

    dummy header component is created

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,50 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Header :generalInfo="generalInfo" />
+  </div>
 </template>
 
 <script>
+const BASEURL = "https://covid19.mathdro.id/api/";
+
+import axios from "axios";
+import Header from "./components/Header.vue";
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      generalInfo: {
+        confirmed: null,
+        recovered: null,
+        deaths: null,
+        lastUpdate: null
+      },
+      countries:{}
+    };
+  },
+  components: {
+    Header
+  },
+  async created() {
+    const fetchTotalValues = async () => {
+      const result = await axios.get(BASEURL);
+      const { data } = result;
+      const { confirmed, recovered, deaths, lastUpdate } = data;
+      this.generalInfo.confirmed = confirmed.value;
+      this.generalInfo.recovered = recovered.value;
+      this.generalInfo.deaths = deaths.value;
+      this.generalInfo.lastUpdate = lastUpdate;
+    };
+    const fetchCountries = async () => {
+      const result = await axios.get(`${BASEURL}countries`);
+      const {
+        data: { countries }
+      } = result;
+      this.countries = countries;
+    };
+    fetchTotalValues();
+    fetchCountries();
+  }
 };
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header-container">
+    <h2>{{ new Date(generalInfo.lastUpdate) }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    generalInfo: Object
+  }
+};
+</script>
+
+<style scoped>
+.header-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  border: 1px solid #ececec;
+}
+</style>
